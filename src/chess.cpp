060a63a3f29c6f67cce963e1ffb2c42e27228073
James McCarthy <blizzard1445@gmail.com>
2014-11-08 17:03:25 -0800

    logic changes, needs review

--- src/chess.cpp
+++ src/chess.cpp
@@ -1,19 +1,16 @@
 #include "chess.h"
+#include<iostream>
 
 typedef Piece::PieceType PType;
 typedef Piece::PieceColor PColor;
 
 Piece::Piece(Board *board, PieceType type, PieceColor color, int x, int y):
     board_(board),
-    type_(type),
     x_(x),
     y_(y),
     color_(color)
 {
-}
 
-Piece::~Piece()
-{
 }
 
 bool Piece::canMove(Piece *piece, int x, int y)
@@ -23,32 +20,116 @@ bool Piece::canMove(Piece *piece, int x, int y)
     switch (piece->type_) {
         case (PType::PAWN):
         {
-
+            //pawn double move first turn privelege
+            if ((piece->y_ == 6) && (piece->color_ == COLOR::WHITE))
+            {
+                if ((y <= piece->y_ - 2) && (y!=0))
+                {
+                   return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if(piece->color_ == COLOR::WHITE)
+            {
+                if (y == piece->y_ - 1)
+                {
+                   return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            if ((piece->y_ == 1) && (piece->color_ == COLOR::BLACK))
+            {
+                if (y >= piece->y_ + 2) and (y!=0)
+                {
+                   return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+             else if(piece->color_ == COLOR::BLACK)
+            {
+                if (y == piece->y_ + 1)
+                {
+                   return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
             break;
         }
         case (PType::ROOK):
         {
             // If rook is moving in only one axis
-            return !(((piece->x_ - x) != 0) && ((piece->y_ - y) != 0));
+            return ((x-piece->x_ == 0)^^(y-piece->y_==0));
         }
         case (PType::KNIGHT):
         {
-
+            if (x == (piece->x_ + 1) && (y== (p->y_ - 2))
+            {
+                return true;
+            }
+            if (x == (piece->x_ + 1) && (y== (p->y_ + 2))
+            {
+                return true;
+            }
+            if (x == (piece->x_ - 1) && (y== (p->y_ - 2))
+            {
+                return true;
+            }
+            if (x == (piece->x_ - 1) && (y== (p->y_ + 2))
+            {
+                return true;
+            }
+            if (x == (piece->x_ + 2) && (y== (p->y_ - 1))
+            {
+                return true;
+            }
+            if (x == (piece->x_ + 2) && (y== (p->y_ + 1))
+            {
+                return true;
+            }
+            if (x == (piece->x_ - 2) && (y== (p->y_ - 1))
+            {
+                return true;
+            }
+            if (x == (piece->x_ - 2) && (y== (p->y_ +1))
+            {
+                return true;
+            }
+            return false;
             break;
         }
         case (PType::BISHOP):
         {
-
+            return ((x - piece->x_) == (y - piece->y_));
             break;
         }
         case (PType::QUEEN):
         {
-
+            return ( ((x - piece->x_) == (y - piece->y_)) || ((x-piece->x == 0)^^(y-piece->y==0)) );
             break;
         }
         case (PType::KING):
         {
-
+            if ((x == 1) && (y == 1))
+            {
+                return ( ((x - piece->x_) == (y - piece->y_)) || ((x-piece->x == 0)^^(y-piece->y==0)) );
+            }
+            else
+            {
+                return false;
+            }
             break;
         }
     }
@@ -58,61 +139,57 @@ bool Piece::canMove(Piece *piece, int x, int y)
 
 Board::Board()
 {
-    for (int y = 0; y < 8; y++) {
-        for (int x = 0; x < 8; x++) {
-            board[y][x] = nullptr;
-        }
-    }
+   Board::setup();
 }
 
 void Board::setup()
 {
     // Create new piece instances
-    pieces[BLACK_ROOK1]   = new Piece(this, PType::ROOK, PColor::BLACK, 0, 0);
-    pieces[BLACK_ROOK2]   = new Piece(this, PType::ROOK, PColor::BLACK, 1, 0);
-    pieces[BLACK_KNIGHT1] = new Piece(this, PType::KNIGHT, PColor::BLACK, 2, 0);
-    pieces[BLACK_KNIGHT2] = new Piece(this, PType::KNIGHT, PColor::BLACK, 3, 0);
-    pieces[BLACK_BISHOP1] = new Piece(this, PType::BISHOP, PColor::BLACK, 4, 0);
-    pieces[BLACK_BISHOP2] = new Piece(this, PType::BISHOP, PColor::BLACK, 5, 0);
-    pieces[BLACK_QUEEN]   = new Piece(this, PType::QUEEN, PColor::BLACK, 6, 0);
-    pieces[BLACK_KING]    = new Piece(this, PType::KING, PColor::BLACK, 7, 0);
+    pieces[BLACK_ROOK1]   = new Piece(PType::ROOK, PColor::BLACK, 0, 0);
+    pieces[BLACK_ROOK1]   = new Piece(PType::ROOK, PColor::BLACK, 1, 0);
+    pieces[BLACK_KNIGHT2] = new Piece(PType::KNIGHT, PColor::BLACK, 2, 0);
+    pieces[BLACK_KNIGHT2] = new Piece(PType::KNIGHT, PColor::BLACK, 3, 0);
+    pieces[BLACK_BISHOP2] = new Piece(PType::BISHOP, PColor::BLACK, 4, 0);
+    pieces[BLACK_BISHOP2] = new Piece(PType::BISHOP, PColor::BLACK, 5, 0);
+    pieces[BLACK_QUEEN]   = new Piece(PType::QUEEN, PColor::BLACK, 6, 0);
+    pieces[BLACK_KING]    = new Piece(PType::KING, PColor::BLACK, 7, 0);
 
-    pieces[BLACK_PAWN1]   = new Piece(this, PType::PAWN, PColor::BLACK, 0, 1);
-    pieces[BLACK_PAWN2]   = new Piece(this, PType::PAWN, PColor::BLACK, 1, 1);
-    pieces[BLACK_PAWN3]   = new Piece(this, PType::PAWN, PColor::BLACK, 2, 1);
-    pieces[BLACK_PAWN4]   = new Piece(this, PType::PAWN, PColor::BLACK, 3, 1);
-    pieces[BLACK_PAWN5]   = new Piece(this, PType::PAWN, PColor::BLACK, 4, 1);
-    pieces[BLACK_PAWN6]   = new Piece(this, PType::PAWN, PColor::BLACK, 5, 1);
-    pieces[BLACK_PAWN7]   = new Piece(this, PType::PAWN, PColor::BLACK, 6, 1);
-    pieces[BLACK_PAWN8]   = new Piece(this, PType::PAWN, PColor::BLACK, 7, 1);
+    pieces[BLACK_PAWN1]   = new Piece(PType::PAWN, PColor::BLACK, 0, 1);
+    pieces[BLACK_PAWN2]   = new Piece(PType::PAWN, PColor::BLACK, 1, 1);
+    pieces[BLACK_PAWN3]   = new Piece(PType::PAWN, PColor::BLACK, 2, 1);
+    pieces[BLACK_PAWN4]   = new Piece(PType::PAWN, PColor::BLACK, 3, 1);
+    pieces[BLACK_PAWN5]   = new Piece(PType::PAWN, PColor::BLACK, 4, 1);
+    pieces[BLACK_PAWN6]   = new Piece(PType::PAWN, PColor::BLACK, 5, 1);
+    pieces[BLACK_PAWN7]   = new Piece(PType::PAWN, PColor::BLACK, 6, 1);
+    pieces[BLACK_PAWN8]   = new Piece(PType::PAWN, PColor::BLACK, 7, 1);
 
-    pieces[WHITE_PAWN1]   = new Piece(this, PType::PAWN, PColor::WHITE, 0, 6);
-    pieces[WHITE_PAWN2]   = new Piece(this, PType::PAWN, PColor::WHITE, 1, 6);
-    pieces[WHITE_PAWN3]   = new Piece(this, PType::PAWN, PColor::WHITE, 2, 6);
-    pieces[WHITE_PAWN4]   = new Piece(this, PType::PAWN, PColor::WHITE, 3, 6);
-    pieces[WHITE_PAWN5]   = new Piece(this, PType::PAWN, PColor::WHITE, 4, 6);
-    pieces[WHITE_PAWN6]   = new Piece(this, PType::PAWN, PColor::WHITE, 5, 6);
-    pieces[WHITE_PAWN7]   = new Piece(this, PType::PAWN, PColor::WHITE, 6, 6);
-    pieces[WHITE_PAWN8]   = new Piece(this, PType::PAWN, PColor::WHITE, 7, 6);
+    pieces[WHITE_PAWN1]   = new Piece(PType::PAWN, PColor::WHITE, 0, 6);
+    pieces[WHITE_PAWN2]   = new Piece(PType::PAWN, PColor::WHITE, 1, 6);
+    pieces[WHITE_PAWN3]   = new Piece(PType::PAWN, PColor::WHITE, 2, 6);
+    pieces[WHITE_PAWN4]   = new Piece(PType::PAWN, PColor::WHITE, 3, 6);
+    pieces[WHITE_PAWN5]   = new Piece(PType::PAWN, PColor::WHITE, 4, 6);
+    pieces[WHITE_PAWN6]   = new Piece(PType::PAWN, PColor::WHITE, 5, 6);
+    pieces[WHITE_PAWN7]   = new Piece(PType::PAWN, PColor::WHITE, 6, 6);
+    pieces[WHITE_PAWN8]   = new Piece(PType::PAWN, PColor::WHITE, 7, 6);
 
-    pieces[WHITE_ROOK1]   = new Piece(this, PType::ROOK, PColor::WHITE, 0, 7);
-    pieces[WHITE_ROOK2]   = new Piece(this, PType::ROOK, PColor::WHITE, 1, 7);
-    pieces[WHITE_KNIGHT1] = new Piece(this, PType::KNIGHT, PColor::WHITE, 2, 7);
-    pieces[WHITE_KNIGHT2] = new Piece(this, PType::KNIGHT, PColor::WHITE, 3, 7);
-    pieces[WHITE_BISHOP1] = new Piece(this, PType::BISHOP, PColor::WHITE, 4, 7);
-    pieces[WHITE_BISHOP2] = new Piece(this, PType::BISHOP, PColor::WHITE, 5, 7);
-    pieces[WHITE_QUEEN]   = new Piece(this, PType::QUEEN, PColor::WHITE, 6, 7);
-    pieces[WHITE_KING]    = new Piece(this, PType::KING, PColor::WHITE, 7, 7);
+    pieces[WHITE_ROOK1]   = new Piece(PType::ROOK, PColor::WHITE, 0, 7);
+    pieces[WHITE_ROOK1]   = new Piece(PType::ROOK, PColor::WHITE, 1, 7);
+    pieces[WHITE_KNIGHT2] = new Piece(PType::KNIGHT, PColor::WHITE, 2, 7);
+    pieces[WHITE_KNIGHT2] = new Piece(PType::KNIGHT, PColor::WHITE, 3, 7);
+    pieces[WHITE_BISHOP2] = new Piece(PType::BISHOP, PColor::WHITE, 4, 7);
+    pieces[WHITE_BISHOP2] = new Piece(PType::BISHOP, PColor::WHITE, 5, 7);
+    pieces[WHITE_QUEEN]   = new Piece(PType::QUEEN, PColor::WHITE, 6, 7);
+    pieces[WHITE_KING]    = new Piece(PType::KING, PColor::WHITE, 7, 7);
 
     // Populate board matrix
     board[0][0] = pieces[BLACK_ROOK1];
-    board[0][1] = pieces[BLACK_KNIGHT1];
-    board[0][2] = pieces[BLACK_BISHOP1];
-    board[0][3] = pieces[BLACK_QUEEN];
-    board[0][4] = pieces[BLACK_KING];
+    board[0][1] = pieces[BLACK_ROOK2];
+    board[0][2] = pieces[BLACK_KNIGHT1];
+    board[0][3] = pieces[BLACK_KNIGHT2];
+    board[0][4] = pieces[BLACK_BISHOP1];
     board[0][5] = pieces[BLACK_BISHOP2];
-    board[0][6] = pieces[BLACK_KNIGHT2];
-    board[0][7] = pieces[BLACK_ROOK2];
+    board[0][6] = pieces[BLACK_QUEEN];
+    board[0][7] = pieces[BLACK_KING];
 
     board[1][0] = pieces[BLACK_PAWN1];
     board[1][1] = pieces[BLACK_PAWN2];
@@ -133,19 +210,58 @@ void Board::setup()
     board[6][7] = pieces[WHITE_PAWN8];
 
     board[7][0] = pieces[WHITE_ROOK1];
-    board[7][1] = pieces[WHITE_KNIGHT1];
-    board[7][2] = pieces[WHITE_BISHOP1];
-    board[7][3] = pieces[WHITE_QUEEN];
-    board[7][4] = pieces[WHITE_KING];
+    board[7][1] = pieces[WHITE_ROOK2];
+    board[7][2] = pieces[WHITE_KNIGHT1];
+    board[7][3] = pieces[WHITE_KNIGHT2];
+    board[7][4] = pieces[WHITE_BISHOP1];
     board[7][5] = pieces[WHITE_BISHOP2];
-    board[7][6] = pieces[WHITE_KNIGHT2];
-    board[7][7] = pieces[WHITE_ROOK2];
+    board[7][6] = pieces[WHITE_QUEEN];
+    board[7][7] = pieces[WHITE_KING];
+
+    // Add pieces to live pieces list
+    live_pieces.push_back(pieces[BLACK_PAWN1]);
+    live_pieces.push_back(pieces[BLACK_PAWN2]);
+    live_pieces.push_back(pieces[BLACK_PAWN3]);
+    live_pieces.push_back(pieces[BLACK_PAWN4]);
+    live_pieces.push_back(pieces[BLACK_PAWN5]);
+    live_pieces.push_back(pieces[BLACK_PAWN6]);
+    live_pieces.push_back(pieces[BLACK_PAWN7]);
+    live_pieces.push_back(pieces[BLACK_PAWN8]);
+
+    live_pieces.push_back(pieces[BLACK_ROOK1]);
+    live_pieces.push_back(pieces[BLACK_ROOK2]);
+    live_pieces.push_back(pieces[BLACK_KNIGHT1]);
+    live_pieces.push_back(pieces[BLACK_KNIGHT2]);
+    live_pieces.push_back(pieces[BLACK_BISHOP1]);
+    live_pieces.push_back(pieces[BLACK_BISHOP2]);
+    live_pieces.push_back(pieces[BLACK_QUEEN]);
+    live_pieces.push_back(pieces[BLACK_KING]);
+
+    live_pieces.push_back(pieces[WHITE_PAWN1]);
+    live_pieces.push_back(pieces[WHITE_PAWN2]);
+    live_pieces.push_back(pieces[WHITE_PAWN3]);
+    live_pieces.push_back(pieces[WHITE_PAWN4]);
+    live_pieces.push_back(pieces[WHITE_PAWN5]);
+    live_pieces.push_back(pieces[WHITE_PAWN6]);
+    live_pieces.push_back(pieces[WHITE_PAWN7]);
+    live_pieces.push_back(pieces[WHITE_PAWN8]);
+
+    live_pieces.push_back(pieces[WHITE_ROOK1]);
+    live_pieces.push_back(pieces[WHITE_ROOK2]);
+    live_pieces.push_back(pieces[WHITE_KNIGHT1]);
+    live_pieces.push_back(pieces[WHITE_KNIGHT2]);
+    live_pieces.push_back(pieces[WHITE_BISHOP1]);
+    live_pieces.push_back(pieces[WHITE_BISHOP2]);
+    live_pieces.push_back(pieces[WHITE_QUEEN]);
+    live_pieces.push_back(pieces[WHITE_KING]);
 }
 
 void Board::cleanup()
 {
-    for (Piece *p : pieces) {
-        delete p;
+    auto it = live_pieces.begin();
+    while (it != live_pieces.end()) {
+        it = live_pieces.erase(it);
+        it++;
     }
 }
 
@@ -159,59 +275,39 @@ bool Board::canMove(int x1, int y1, int x2, int y2)
     return Piece::canMove(board[y1][x1], x2, y2);
 }
 
-void Board::printBoard(){
-    for (int y = 0; y < 8; y++) {
-        for (int x = 0; x < 8; x++) {
-            if (board[y][x] != nullptr) {
-                switch (board[y][x]->type_) {
-                    case (PType::PAWN):
-                        cout << 'P';
-                        break;
-                    case (PType::ROOK):
-                        cout << 'R';
-                        break;
-                    case (PType::KNIGHT):
-                        cout << 'k';
-                        break;
-                    case (PType::BISHOP):
-                        cout << 'B';
-                        break;
-                    case (PType::QUEEN):
-                        cout << 'Q';
-                        break;
-                    case (PType::KING):
-                        cout << 'K';
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        cout << endl;
-    }
-    cout << endl;
+void Board::printBoardArray(){
+   //for(int i = 0; i < 8; i++){
+      //for(int j = 0; j < 8; j++){
+         //if(board[i][j]){
+            //cout << "x  ";
+         //}
+         //else{
+            //cout << board[i][j]->type_ << "  ";
+         //}
+      //}
+      //cout << endl;
+   //}
+   //cout << endl;
 }
 
 int main(){
-    //string email = "test@test.com";
+   //string email = "test@test.com";
 
-    Board b;
+   //Board b;
 
-    b.setup();
+   //b.printBoardArray();
 
-    b.printBoard();
+   //b.move(email,0,4,4,4);
 
-    //b.move(email,0,4,4,4);
+   //b.printBoardArray();
 
-    //b.printBoardArray();
+   //b.move(email,1,3,5,5);
 
-    //b.move(email,1,3,5,5);
+   //b.printBoardArray();
 
-    //b.printBoardArray();
+   //cin.ignore();
 
-    cin.ignore();
-
-    return EXIT_SUCCESS;
+   return 0;
 }
 
 
